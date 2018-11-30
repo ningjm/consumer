4eec2a4c357a2323be8dbf4ab7f2968cb51a082c
ningjm <2719334283@qq.com>
2018-11-30 11:57:04 +0800

    如果业务逻辑出现问题（比如网络突然中断）------重新消费

--- src/main/java/com/juzhen/consumer/ShopConsumer.java
+++ src/main/java/com/juzhen/consumer/ShopConsumer.java
@@ -3,10 +3,12 @@ package com.juzhen.consumer;
 import com.juzhen.producer.pojo.Shop;
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -28,6 +30,27 @@ public class ShopConsumer {
     )
     @RabbitHandler
     public void onShopListen(@Payload Shop shop, @Headers Map<String,Object> headers, Channel channel){
-        System.out.println(shop.getCategory());
+
+        try {
+            //业务逻辑---如操作数据库失败
+            int a = 1/0;
+        }catch (Exception ex){
+            try {
+                //否认消息--消息重新投递
+                channel.basicNack((Long)headers.get(AmqpHeaders.DELIVERY_TAG), false,true);
+                System.out.println("否认消息--消息重新消费");
+                return;//退出方法
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            //业务成功，消息确认消费
+            channel.basicAck((Long)headers.get(AmqpHeaders.DELIVERY_TAG), false);//确认消息-消费消息
+            System.out.println("业务成功，消息确认消费");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
